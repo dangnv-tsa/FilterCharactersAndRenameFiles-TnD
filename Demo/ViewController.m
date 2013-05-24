070cbbb30f2fc2a07057620c671baeda63b22cd6
dangnv <dangnv.ict@gmail.com>
2013-05-24 13:43:42 +0700

    Initial Project

--- Demo/ViewController.m
+++ Demo/ViewController.m
@@ -13,11 +13,12 @@
 @end
 
 @implementation ViewController
-
+@synthesize txtRenamed;
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	// Do any additional setup after loading the view, typically from a nib.    
+    [self renameFiles];
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +26,55 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (void)renameFiles{
+    txtRenamed.hidden = TRUE;
+    NSString *documentsDirectory = @"/Users/dangnv/Desktop/untitled folder/"; //Enter path of directory to rename
+    
+    NSFileManager *manager = [NSFileManager defaultManager];
+    NSArray *fileList = [manager contentsOfDirectoryAtPath:documentsDirectory error:nil];
+    for (NSString *s in fileList){
+        NSString *checkStr = [[s stringByReplacingOccurrencesOfString:@".png" withString:@""] stringByReplacingOccurrencesOfString:@"@2x" withString:@""];
+        
+        NSString *newName = [self filterStr:checkStr withCharacters:@"0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+        if ([s rangeOfString:@"@2x"].location != NSNotFound) {
+            newName = [NSString stringWithFormat:@"%@%@", newName,@"@2x"];
+        }
+        
+        NSLog(@"%@/%@", s, newName);
+        
+        NSString *newPath = [NSString stringWithFormat:@"%@/%@.png", documentsDirectory, newName];
+        NSString *oldPath = [NSString stringWithFormat:@"%@/%@", documentsDirectory, s];
+        [[NSFileManager defaultManager] moveItemAtPath:oldPath toPath:newPath error:nil];
+    }
+    
+    txtRenamed.hidden = FALSE;
+
+}
+
+
+- (NSString *)filterStr:(NSString *)str withCharacters:(NSString *)charSet{
+//    NSString *inputStr = [str lowercaseString];
+    NSString *inputStr = str;
+    NSMutableString *strippedString = [NSMutableString
+                                       stringWithCapacity:inputStr.length];
+    
+    NSScanner *scanner = [NSScanner scannerWithString:inputStr];
+    NSCharacterSet *filter = [NSCharacterSet
+                               characterSetWithCharactersInString:charSet];
+    
+    while ([scanner isAtEnd] == NO) {
+        NSString *buffer;
+        if ([scanner scanCharactersFromSet:filter intoString:&buffer]) {
+            [strippedString appendString:buffer];
+            
+        } else {
+            [scanner setScanLocation:([scanner scanLocation] + 1)];
+        }
+    }
+    
+    return strippedString;
+}
+
 
 @end
